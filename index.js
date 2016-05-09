750622cf369516bd95454c14fa31ac6dd15eb9e0
julien andre <julien.andre@ariadnext.com>
2016-05-09 17:32:40 +0200

    first program launched.

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
 #! /usr/bin/env node
+/**
+ Module permettant de générer un UID unique depuis l'ID Hardware du RPi
+ */
+var fs = require('fs');
+var os = require('os');
+var bases = require('bases');
+var program = require('commander');
 
-console.log('This is a test for Hostname Generator.');
+
+// PRIVATE
+var genHostnameUID = '';
+var baseHostname = 'Station-';
+
+
+program
+  .version('1.0.0')
+  .usage( '[options]' )
+  .option('-f, --forcehostname [hostname]', 'force a special hostname to be setup.')
+  .option('-s, --setup', 'setup the generated hostname', setup )
+  .parse(process.argv);
+
+
+
+function getHostname()
+{
+  // read the /proc/cpuinfo file and
+  fs.readFile('/proc/cpuinfo', 'utf8', function handleCPUInfos (err, data) {
+    if (err) {
+      console.log('Error reading the cpuinfo file : %s', JSON.stringify(err, 0, 2));
+    } else {
+      // read the serial characters ( 16 last chars )
+      var serialID = data.slice(-17, -1);
+      console.log('-- Raw CPU Serial ID retrieved : %s', serialID);
+      genHostnameUID = baseHostname + bases.toBase32(bases.fromBase16(serialID));
+      console.log('-- Hostname generated : ' + genHostnameUID);
+    }
+  });
+}
+getHostname();
+
+/**
+ * Setup the hostname
+ */
+function setup(){
+  var newHostname = ( program.forcehostname )?  program.forcehostname : genHostnameUID;
+  var oldHostname = fs.readFileSync('/etc/hostname', 'utf8').trim();
+  console.log('-- Seting up Hostname from '+oldHostname+' to '+ newHostname);
+  fs.writeFileSync('/etc/hostname', newHostName );
+  var hostsFile = fs.readFileSync('/etc/hosts', 'utf8');
+  var hostsFile2 = hostsFile.replace( oldHostname, newHostName );
+  fs.writeFileSync( '/etc/hosts', hostsFile2 );
+  console.log('-- Seting up Hostname DONE, you have to reboot to take effect !');
+}
+
